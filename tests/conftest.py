16e3d797bd18cef0d23fca22ec5173cb9623dfcd
Rene Beckmann <rene.bckmnn@gmail.com>
2021-04-10 23:08:36 +0200

    Sort files when creating tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,7 +6,7 @@ _diagrams = os.path.join(_path, "diagrams")
 
 def pytest_generate_tests(metafunc):
     if "file" in metafunc.fixturenames:
-        files = os.listdir(_diagrams)
+        files = sorted(os.listdir(_diagrams))
         diagrams = [f for f in files if f.endswith(".pu")]
         diagrams = map(lambda f: os.path.join(_diagrams, f), diagrams)
         metafunc.parametrize("file", diagrams)
