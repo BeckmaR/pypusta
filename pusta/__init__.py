8c0c954fb71622fbf8bbf6f81443abbd196ab012
Rene Beckmann <rene.bckmnn@gmail.com>
2021-04-10 22:48:40 +0200

    Improve parsing of descrioption strings

--- pusta/__init__.py
+++ pusta/__init__.py
@@ -18,7 +18,7 @@ class Pusta:
     _grammar_path = os.path.join(os.path.dirname(__file__), 'state.tx')
 
     def __init__(self):
-        self._parser = textx.metamodel_from_file(self._grammar_path)
+        self._parser = textx.metamodel_from_file(self._grammar_path, use_regexp_group=True)
 
     def parse(self, s):
         return Diagram(self._parser.model_from_str(s))
