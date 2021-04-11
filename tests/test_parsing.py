3717942c4c4f4b4fadfe854d23fecb39f7affcac
Rene Beckmann <rene.bckmnn@gmail.com>
2021-04-11 15:15:20 +0200

    Clean up parsing test

--- tests/test_parsing.py
+++ tests/test_parsing.py
@@ -1,13 +1,7 @@
 import pusta
-import logging
-import os
-
-logger = logging.getLogger(__name__)
 
 parser = pusta.Pusta()
 
 
 def test_parsing(file):
-    logger.debug(file)
-
     diagram = parser.parse_file(file)
